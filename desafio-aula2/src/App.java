981651d57bf288f7e6e13c4e2b7ab44401d2f5e7
carvalhaus <ooliveira.joaop@gmail.com>
2023-03-29 17:45:15 -0300

    Desafio Imersão Java Alura Aula 2

--- desafio-aula2/src/App.java
+++ desafio-aula2/src/App.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
@@ -5,8 +7,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 
@@ -30,25 +30,35 @@ public class App {
         String emojiTv = Character.toString(0x1f4fa);
         String emojiStar = Character.toString(0x2b50);
 
-        System.out.println("\u001b[1m===== Gerando Figurinhas dos Top Filmes =====\u001b[m \n");
+        System.out.println("\u001b[1m===== Gerando Figurinhas dos Filmes =====\u001b[m \n");
 
-        String path = "saida";
-        Files.createDirectory(Paths.get(path));
-
+        File path = new File("saida/");
+        path.mkdir();
+        
         for (Map<String,String> top : topMovies) {
             String urlImagem = top.get("image");
             String titulo = top.get("title");
             String nomeArquivo = "saida/" + titulo + ".png";
             InputStream inputStream = new URL(urlImagem).openStream();
+            
 
             System.out.println(emojiTv + " Título do filme: \u001b[3m" + titulo + "\u001b[m");
-            //System.out.println("Poster da série: " + top.get("image"));
             double ratingDb = Double.parseDouble(top.get("imDbRating")); //convertendo String em Double
             int rate = (int) ratingDb; //convertendo Double em int
             System.out.println("Classificação " + emojiStar.repeat(rate) + "\n");
+            
+            String textoFigurinha;
+            InputStream stamp;
+            if (rate >= 9) {
+                textoFigurinha = "RECOMENDO!";
+                stamp = new FileInputStream(new File("resource/stamp-approved.png"));
+            } else {
+                textoFigurinha = "NÃO ASSISTA!";
+                stamp = new FileInputStream(new File("resource/stamp-denied.png"));
+            }
 
             GeradorFigurinhas geradora = new GeradorFigurinhas();
-            geradora.criaFigurinhas(inputStream, nomeArquivo);
+            geradora.criaFigurinhas(inputStream, nomeArquivo, textoFigurinha, stamp);
         }
 
     }
--- desafio-aula2/src/GeradorFigurinhas.java
+++ desafio-aula2/src/GeradorFigurinhas.java
@@ -1,8 +1,13 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.Shape;
 import java.awt.font.FontRenderContext;
 import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.InputStream;
@@ -13,11 +18,11 @@ public class GeradorFigurinhas{
     
     
     
-    public void criaFigurinhas(InputStream inputStream, String nomeArquivo) throws Exception {
+    public void criaFigurinhas(InputStream inputStream, String nomeArquivo, String texto, InputStream inputStream2) throws Exception {
         
+        //leitura das imagens
         BufferedImage imagemOriginal = ImageIO.read(inputStream);
-        //Image imagemMenor = imagemOriginal.getScaledInstance(600, 835, Image.SCALE_SMOOTH);
-
+        
         // cria nova imagem em memória com transparência e com tamanho novo
         int largura = imagemOriginal.getWidth();
         int altura = imagemOriginal.getHeight();
@@ -28,22 +33,45 @@ public class GeradorFigurinhas{
         Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
         graphics.drawImage(imagemOriginal, 0, 0, null);
 
+        BufferedImage stampRate = ImageIO.read(inputStream2);
+        int posicaoStamp = (altura - stampRate.getHeight());
+        graphics.drawImage(stampRate, 0, posicaoStamp, null);
+
         // configurar fonte
         Font fontComic = Font.createFont(Font.TRUETYPE_FONT,
-            new File("fonts/ComicNeue-Bold.ttf"))
-            .deriveFont(Font.BOLD, 66);
+            new File("resource/ComicNeue-Bold.ttf"))
+            .deriveFont(Font.BOLD, 88);
         graphics.setColor(Color.ORANGE);
         graphics.setFont(fontComic);
 
-        FontRenderContext fontRenderContext = graphics.getFontRenderContext();
-        new TextLayout(null, fontRenderContext)
-
-
+        
+        
         // escrever uma frase na nova imagem
-        graphics.drawString("TOPZERA", 185, novaAltura - 80);
+        FontMetrics fontMetrics = graphics.getFontMetrics();
+        Rectangle2D retanguloTexto = fontMetrics.getStringBounds(texto, graphics);
+        int larguraTexto = (int) retanguloTexto.getWidth();
+        int posicaoTextoX = (largura - larguraTexto) / 2;
+        int posicaoTextoY = novaAltura - 80;
+        graphics.drawString(texto, posicaoTextoX, posicaoTextoY );
+        graphics.drawImage(novaImagem, altura, novaAltura, null);
+        
+        // outline da frase
+        FontRenderContext fontRenderContext = graphics.getFontRenderContext();
+        TextLayout textLayout = new TextLayout(texto, fontComic, fontRenderContext);
+        
+        Shape outline = textLayout.getOutline(null);
+        AffineTransform transform = graphics.getTransform();
+        transform.translate(posicaoTextoX, posicaoTextoY);
+        graphics.setTransform(transform);
 
+        BasicStroke outlineStroke = new BasicStroke(largura * 0.004f);
+        graphics.setStroke(outlineStroke);
+
+        graphics.setColor(Color.BLACK);
+        graphics.draw(outline);
+        graphics.setClip(outline);
+        
         // escrever a nova imagem em um arquivo
-
         ImageIO.write(novaImagem, "png", new File(nomeArquivo));
 
     }
